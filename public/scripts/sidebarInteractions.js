b565eabf011dbe3e6a56acc79b1281f75b615c67
lymyr <176290600+lymyr@users.noreply.github.com>
2026-08-24 14:18:35 +0800

    styles: changed sidebar interactions

--- public/scripts/sidebarInteractions.js
+++ public/scripts/sidebarInteractions.js
@@ -1,12 +1,6 @@
 const aside = document.querySelector('aside')
-
-const asideBtn = document.querySelector('aside button')
-const headerBtn = document.querySelector('header button')
+const asideBtn = document.querySelector('.aside-btn button')
 
 asideBtn.addEventListener('click', () => {
-    aside.classList.add('hide')
-})
-
-headerBtn.addEventListener('click', () => {
     aside.classList.toggle('hide')
 })
